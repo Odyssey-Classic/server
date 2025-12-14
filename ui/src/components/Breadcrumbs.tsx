d20369af490b7cf9a677f56c7d1b33b197d10e05
Christopher Lowenthal <clowenhg@gmail.com>
2025-12-13 20:31:48 -0500

    removes breadcrumb bar resizing logic

--- ui/src/components/Breadcrumbs.tsx
+++ ui/src/components/Breadcrumbs.tsx
@@ -4,21 +4,6 @@ import { useTitleBar } from '../contexts/TitleBarContext'
 export default function Breadcrumbs() {
     const { breadcrumbs } = useTitleBar()
 
-    useEffect(() => {
-        const content = document.querySelector('.admin-content') as HTMLElement
-        if (content) {
-            if (breadcrumbs.length > 0) {
-                content.style.marginTop = 'calc(var(--title-bar-height) + var(--breadcrumbs-height))'
-            } else {
-                content.style.marginTop = 'var(--title-bar-height)'
-            }
-        }
-    }, [breadcrumbs.length])
-
-    if (breadcrumbs.length === 0) {
-        return null
-    }
-
     return (
         <div className="breadcrumbs">
             {breadcrumbs.map((crumb, index) => (
